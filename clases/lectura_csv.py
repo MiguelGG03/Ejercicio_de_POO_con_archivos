2b79ee495d88bb97da4e8aa8989016eeb5d37c0e
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-08 17:57:17 +0200

    cambios

--- clases/lectura_csv.py
+++ clases/lectura_csv.py
@@ -1,10 +1,10 @@
-import pandas as pd
-
 class lectura:
     def __init__(self,ubi):
         lector=open(ubi,'r',encoding='utf-8')
         datos=[]
         lista_datos=[]
+        lista_notas=[]
+        self.lista_notas=lista_notas
         self.lector=lector
         self.datos=datos
         self.lista_datos=lista_datos
@@ -15,7 +15,6 @@ class lectura:
             i=i.rstrip('\n')
             colm= i.split(';')
             self.datos.append(colm)
-        print(self.datos)
         return self.datos
 
     def lista_con_nna(self):   #Nombres,notas y asistencias
@@ -28,9 +27,81 @@ class lectura:
                   self.datos[i][6],
                   self.datos[i][7],
                   self.datos[i][8]]
-                  
-            self.lista_datos.append(temp)
-        print(self.lista_datos)
 
-    def 
-    
+            self.lista_datos.append(temp)
+
+    def medias(self):
+        for i in range(1,17):
+            x=0
+            if(self.datos[i][3]==''):
+                if(self.datos[i][5]==''):
+                    examen_1=0
+                else:
+                    examen_1=float(self.datos[i][5].replace(',','.'))
+            else:
+                    if(float(self.datos[i][3].replace(',','.'))>4):
+                        examen_1=float(self.datos[i][3].replace(',','.'))
+                    else:
+                        if(self.datos[i][5]==''):
+                            examen_1=0
+                        else:
+                            examen_1=float(self.datos[i][5].replace(',','.'))
+
+
+            if(self.datos[i][4]==''):
+                if(self.datos[i][6]==''):
+                    examen_2=0
+                else:
+                    examen_2=float(self.datos[i][6].replace(',','.'))
+            else:
+                    if(float(self.datos[i][4].replace(',','.'))>4):
+                        examen_2=float(self.datos[i][4].replace(',','.'))
+                    else:
+                        if(self.datos[i][6]==''):
+                            examen_2=0
+                        else:
+                            examen_2=float(self.datos[i][6].replace(',','.'))
+
+
+            if(self.datos[i][7]==''):
+                if(self.datos[i][8]==''):
+                    examen_3=0
+                else:
+                    examen_3=float(self.datos[i][8].replace(',','.'))
+            else:
+                    if(float(self.datos[i][7].replace(',','.'))>4):
+                        examen_3=float(self.datos[i][7].replace(',','.'))
+                    else:
+                        if(self.datos[i][8]==''):
+                            examen_3=0
+                        else:
+                            examen_3=float(self.datos[i][8].replace(',','.'))
+
+                        """
+                    if(float(self.datos[i][4].replace(',','.'))>4):
+                        examen_2=float(self.datos[i][4].replace(',','.'))
+                    else:
+                        if(self.datos[i][6]==''):
+                            examen_2=0
+                        else:
+                            examen_2=float(self.datos[i][6].replace(',','.')) 
+
+
+                    if(float(self.datos[i][7].replace(',','.'))>4 and self.datos[i][7]!=''):
+                        examen_3=float(self.datos[i][7].replace(',','.'))
+                    else:
+                        if(self.datos[i][8]==''):
+                            examen_3=0
+                        else:
+                            examen_3=float(self.datos[i][8].replace(',','.'))
+
+                        """
+
+            x=((examen_1 + examen_2)*0,3)+(examen_3*0,4)                
+
+
+
+            temp=[self.datos[i][1],self.datos[i][0],x]
+            self.lista_notas.append(temp)
+        
+        return self.lista_notas
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     l=lectura('calificaciones.csv')
     lista_con_todo=l.crear_lista()
     lista_nna=l.lista_con_nna()
+    lista_con_medias=l.medias()
+    print(lista_con_medias)
 
 if __name__=='__main__':
     main()
