4f32fd04f98e76e6c858943c8e93dab2443b48f5
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-08 16:18:37 +0200

    par de cambios

--- clases/lectura_csv.py
+++ clases/lectura_csv.py
@@ -1,17 +1,21 @@
+import pandas as pd
+
 class lectura:
     def __init__(self):
-        f=open('calificaciones.csv','r',encoding='utf-8')
+        lector=open('calificaciones.csv','r',encoding='utf-8')
         datos=[]
         lista_datos=[]
-        datos=[]
-        self.f=f
+        self.lector=lector
         self.datos=datos
         self.lista_datos=lista_datos
         self.diccionario=datos
 
     def crear_lista(self):
-        for i in self.f:
+        for i in self.lector:
             i=i.rstrip('\n')
             colm= i.split(';')
             self.datos.append(colm)
+        print(self.datos)
         return self.datos
+
+    
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from clases.lectura_csv import *
 
 l=lectura()
+l.crear_lista
