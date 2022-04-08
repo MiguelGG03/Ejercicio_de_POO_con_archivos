8a265b31f93060799832f210981c4bca8cfe0306
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-08 09:45:07 +0200

    creando metodos wey

--- clases/lectura_csv.py
+++ clases/lectura_csv.py
@@ -5,3 +5,12 @@ class lectura:
         f = open('calificaciones.csv', 'r',encoding='utf-8')
         datos=[]
         lista_datos=[]
+        self.f=f
+        self.datos=datos
+        self.lista_datos=lista_datos
+
+    def leer_csv(self):
+        with open('calificaciones.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row['first_name'], row['last_name'])
